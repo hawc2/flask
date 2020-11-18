f2d27fda2aae611354ecd4b24c1cfc0edfc8c871
hawc2 <alwermercolan@gmail.com>
2020-11-18 15:34:21 -0500

    creating subpages

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/sf/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/hello/')
 def hello_world():
     return 'Hello, World!'
+
+app.run()
