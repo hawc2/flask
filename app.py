0b38765658387dd2ad6657c6003b177660a69791
hawc2 <alwermercolan@gmail.com>
2020-11-19 11:56:27 -0500

    adding production server

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app(test_config=None):
+	app = Flask(__name__)
+	
+	@app.route('/')
+	def index():
+	    return render_template('index.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+	@app.route('/hello')
+	def hello():
+	    return render_template('hello.html')
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+	return app
